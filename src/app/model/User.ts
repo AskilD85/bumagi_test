a84736627c0851b5996d5345df5059cdde3f8dc4
askild85 <askildar@yandex.ru>
2022-03-20 03:43:41 +0500

    Изменил сущности User и  Status

--- src/app/model/User.ts
+++ src/app/model/User.ts
@@ -1,12 +1,23 @@
 export class User {
+  id:number;
   name: string;
   fname: string;
   mname:string;
-  status:string;
-  constructor (name:string,fname:string,mname:string,status:string) {
+  status:Status;
+  foto_url?: string;
+  full_name?:string;
+  balance?:string|number;
+  last_update?:string;
+  constructor (id:number,name:string,fname:string,mname:string,status: Status) {
+    this.id = id;
     this.name = name;
     this.fname=fname;
     this.mname = mname;
     this.status = status;
   }
+}
+
+export class Status {
+  id?: number;
+  value?: string
 }
